33866c45715a4a02c06d0967322182dec0a8de08
IamSaud <144736130+IamSaud@users.noreply.github.com>
2024-02-15 20:25:48 +0300

    Update Program3.py
    Add a description for prog3

--- IS424_Lab02/Program3.py
+++ IS424_Lab02/Program3.py
@@ -1,6 +1,8 @@
 # Saud AlQahtani
 # 442101254
 
+# Program to convert Celsius temperature to Fahrenheit.
+
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = (9/5) * celsius + 32
 print("Temperature in Fahrenheit:", fahrenheit)
