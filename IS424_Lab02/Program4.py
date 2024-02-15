f9d7b20a83737d79ab2371897e528ad7e00e4e3d
IamSaud <144736130+IamSaud@users.noreply.github.com>
2024-02-15 20:26:36 +0300

    Update Program4.py
    Add a description for prog4

--- IS424_Lab02/Program4.py
+++ IS424_Lab02/Program4.py
@@ -1,6 +1,8 @@
 #Saud AlQahtani
 # 442101254
 
+# Program to print the multiplication table of a number entered by the user.
+
 number = int(input("Enter a number: "))
 
 print("Multiplication Table for", number)
