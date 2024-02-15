b10daba999ba2ea70d963d5f781c7c12062880f6
IamSaud <144736130+IamSaud@users.noreply.github.com>
2024-02-15 20:24:48 +0300

    Update Program2.py
    Add a description for prog2

--- IS424_Lab02/Program2.py
+++ IS424_Lab02/Program2.py
@@ -1,6 +1,8 @@
 # Saud AlQahtani
 # 442101254
 
+# Program to find the largest number in a list of 10 numbers.
+
 numbers = []
 for i in range(10):
     numbers.append(int(input("Enter a number: ")))
